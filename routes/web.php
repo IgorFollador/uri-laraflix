cc75dcac24c9a5e32441e1c8a61b516c4036b506
IgorFollador <igorledf@hotmail.com>
2022-04-06 21:06:38 -0300

    Initial structure for auth

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/', function () {
 });
 
 Route::get('/atores', [AtoresController::class, 'index']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
