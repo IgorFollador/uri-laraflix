e8b8164d1a6ec3d45bd976d2b557bb92994074a9
IgorFollador <igorledf@hotmail.com>
2022-04-06 21:59:46 -0300

    Show Atores in table

--- resources/views/atores.blade.php
+++ resources/views/atores.blade.php
@@ -1,9 +1,21 @@
 @extends('adminlte::page')
 
 @section('content')
-    @foreach($atores as $ator)
-        <li>{{ $ator->nome }}</li>
-        <li>{{ $ator->dt_nascimento }}</li>
-        <br>
-    @endforeach
+    <h1>Atores</h1>
+    <table class="table table-stripe table-bordered table-hover">
+        <thead>
+            <th>Nome</th>
+            <th>Data de Nascimento</th>
+        </thead>
+
+        <tbody>
+            @foreach($atores as $ator)
+                <tr>
+                    <td>{{ $ator->nome }}</td>
+                    <td>{{ $ator->dt_nascimento }}</td>
+                    <br>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @stop
