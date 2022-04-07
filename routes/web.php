e5ecde3747271fda65a4f827afb230f8169c15b3
IgorFollador <igorledf@hotmail.com>
2022-04-06 22:14:14 -0300

    Created route and view to create actors

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AtoresController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/atores', [AtoresController::class, 'index']);
+Route::get('/atores/create', [AtoresController::class, 'create']);
+
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
