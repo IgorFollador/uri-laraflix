c3e7f91522922ecf7d7fd448b96ce80cd1ab86f7
IgorFollador <igorledf@hotmail.com>
2022-04-13 22:09:17 -0300

    Reordering the routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AtoresController;
+// use \App\Http\Controllers\AtoresController;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/atores', [AtoresController::class, 'index']);
-Route::get('/atores/create', [AtoresController::class, 'create']);
-Route::post('/atores/store', [AtoresController::class, 'store']);
-Route::get('atores/{id}/destroy', [AtoresController::class, 'destroy']);
+
+Route::group(['prefix'=>'atores', 'where'=>['id'=>'[0-9]+']], function() {
+    Route::get('',                 ['as'=>'atores',            'uses'=>'\App\Http\Controllers\AtoresController@index'  ]);
+    Route::get('create',           ['as'=>'atores.create',     'uses'=>'\App\Http\Controllers\AtoresController@create' ]);
+    Route::get('{id}/destroy',     ['as'=>'atores.destroy',    'uses'=>'\App\Http\Controllers\AtoresController@destroy']);
+    Route::get('{id}/edit',        ['as'=>'atores.edit',       'uses'=>'\App\Http\Controllers\AtoresController@edit   ']);
+    Route::put('{id}/update',      ['as'=>'atores.update',     'uses'=>'\App\Http\Controllers\AtoresController@update' ]);
+    Route::post('store',           ['as'=>'atores.store',      'uses'=>'\App\Http\Controllers\AtoresController@store'  ]);
+});
 
 Auth::routes();
 
