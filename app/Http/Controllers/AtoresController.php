5851abed5b1b2da91126f8416ca2c8ee091bb02f
IgorFollador <igorledf@hotmail.com>
2022-04-13 21:24:26 -0300

    Validate create Ator in backend

--- app/Http/Controllers/AtoresController.php
+++ app/Http/Controllers/AtoresController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Ator;
+use App\Http\Requests\AtorRequest;
 
 class AtoresController extends Controller
 {
@@ -16,11 +17,12 @@ class AtoresController extends Controller
         return view('atores.create');
     }
 
-    public function store(Request $request){
+    public function store(AtorRequest $request){
         $novo_ator = $request->all();
         // echo $novo_ator;
         Ator::create($novo_ator);
 
         return redirect('atores');
     }
+
 }
--- resources/views/atores/create.blade.php
+++ resources/views/atores/create.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
     <h3>Novo Ator</h3>
+
+    @if($errors->any())
+        <ul class="alert alert-danger">
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
     {!! Form::open(['url'=>'atores/store']) !!}
         
         <div class="form-group">
