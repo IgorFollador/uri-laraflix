c9aa4d917580c8250ee2bd29bf434c30e2cac02f
Igor Follador <igorledf@hotmail.com>
2022-06-15 19:33:00 -0300

    Exclusion control

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,6 +13,9 @@
             }).then(function(isConfirm) {
                 if(isConfirm.value) {
                     $.get('/'+ @yield('table-delete') +'/'+id+'/destroy', function(data) {
+                        //success data
+                        //console.log(data);
+                        if (data.status == 200) {
                         swal.fire(
                             'Deletado!',
                             'Exclusão confirmada.',
@@ -20,6 +23,13 @@
                         ).then(function() {
                             window.location.reload();
                         });
+                    } else {
+                        swal.fire(
+                            'Erro!',
+                            'Ocorreram erros na exclusão. Entre em contato com o suporte.',
+                            'error'
+                            );
+                        }
                     });
                 }
             })
